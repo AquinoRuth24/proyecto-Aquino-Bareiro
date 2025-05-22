db5b0f6c5a3589699b9abffe5a65ac00df3c0224
AquinoRuth24 <ruthaquino100@gmail.com>
2025-05-22 11:08:21 -0300

    cambie el footer

--- app/Views/components/footer.php
+++ app/Views/components/footer.php
@@ -1,4 +1,53 @@
 <footer id="contacto" class="footer">
     <div style="max-width: 1200px; margin: auto;">
         <p>&copy; 2025 Yesi Yohi Store. All rights reserved.</p>
+    </div>
+    <div class="container">
+        <div class="row text-start">
+
+            <!-- Navegación -->
+            <div class="col-md-3 mb-3">
+                <h6 class="fw-bold">NAVEGACIÓN</h6>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white text-decoration-none">Inicio</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Quiénes somos</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Comercialización</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Términos y uso</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Consultas</a></li>
+                </ul>
+            </div>
+
+            <!-- Medios de pago -->
+            <div class="col-md-3 mb-3">
+                <h6 class="fw-bold">MEDIOS DE PAGO</h6>
+                <div class="d-flex flex-wrap gap-2">
+                    <img src="public/assets/img/visa.jpg" alt="visa" width="40">
+                    <img src="public/assets/img/mastercard.jpg" alt="visa" width="40">
+                    <img src="public/assets/img/amex.jpg" alt="visa" width="40">
+                    <img src="public/assets/img/mercado-pago.jpg" alt="visa" width="40">
+                    <img src="public/assets/img/paypal.jpg" alt="visa" width="40">
+                </div>
+            </div>
+
+            <!-- Contacto -->
+            <div class="col-md-3 mb-3">
+                <h6 class="fw-bold">CONTACTANOS</h6>
+                <ul class="list-unstyled">
+                    <li><i class="bi bi-telephone-fill"></i> +54 9 3795-103530</li>
+                    <li><i class="bi bi-telephone-fill"></i> +54 9 379 480-0790</li>
+                    <li><i class="bi bi-envelope-fill"></i> andres.bareiro@outlook.com</li>
+                    <li><i class="bi bi-envelope-fill"></i> aquinoruth2004@gmail.com</li>
+                    <li><i class="bi bi-geo-alt-fill"></i> 9 de julio 1453,Corrientes,Corrientes,Argentina</li>
+                </ul>
+            </div>
+
+            <!-- Redes sociales -->
+            <div class="col-md-3 mb-3 text-center text-md-start">
+                <h6 class="fw-bold">REDES SOCIALES</h6>
+                <img src="public/assets/img/instagram.jpg" alt="visa" width="40">
+                <img src="public/assets/img/logoWpp.jpg" alt="visa" width="40">
+            </div>
+
+        </div>
+    </div>
 </footer>
